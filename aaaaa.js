0f47d36a4491681c3e9268d5c9319d106489b993
cbmonkey9x <62202515+cbmonkey9x@users.noreply.github.com>
2020-03-18 22:58:33 +0700

    Update aaaaa.js

--- aaaaa.js
+++ aaaaa.js
@@ -1,18 +1,70 @@
-eval(function (p, a, c, k, e, r) {
-    e = function (c) {
-        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+$(document).ready(function () {
+    $("pre").each(function () {
+        var _0x4332x2 = $(this).text();
+        var _0x4332x3 = $(this).attr("class");
+        _0x4332x2 = _0x4332x2.replace(/&/g, "&amp;");
+        _0x4332x2 = _0x4332x2.replace(/</g, "&lt;");
+        _0x4332x2 = _0x4332x2.replace(/>/g, "&gt;");
+        _0x4332x2 = _0x4332x2.replace(/"/g, "&quot;");
+        _0x4332x2 = _0x4332x2.replace(/'/g, "&#039;");
+        $(this).html("<copy class=\"" + _0x4332x3 + "\">" + _0x4332x2 + "</copy>")
+    });
+    $("copy").each(function (_0x4332x4, _0x4332x5) {
+        hljs.highlightBlock(_0x4332x5)
+    })
+});
+$(document).ready(function () {
+    var _0x4332x6 = $("pre copy"),
+        _0x4332x7 = "<button class=\"copycode\" alt=\"Chu Bằng Blogger - chubangblogger.blogspot.com\" title=\"Chu Bằng Blogger - chubangblogger.blogspot.com\"><i class=\"fa fa-clipboard\"></i> Copy</button>";
+    _0x4332x6.before(_0x4332x7)
+});
+(function (_0x4332x7) {
+    _0x4332x7.fn.selectText = function () {
+        var _0x4332x8 = document;
+        var _0x4332x9 = this[0];
+        console.log(this, _0x4332x9);
+        if (_0x4332x8.body.createTextRange) {
+            var _0x4332x6 = document.body.createTextRange();
+            _0x4332x6.moveToElementText(_0x4332x9);
+            _0x4332x6.select()
+        } else {
+            if (window.getSelection) {
+                var _0x4332xa = window.getSelection();
+                var _0x4332x6 = document.createRange();
+                _0x4332x6.selectNodeContents(_0x4332x9);
+                _0x4332xa.removeAllRanges();
+                _0x4332xa.addRange(_0x4332x6);
+                document.execCommand("Copy")
+            }
+        }
     };
-    if(!''.replace(/^/, String)) {
-        while(c--) r[e(c)] = k[c] || e(c);
-        k = [function (e) {
-            return r[e]
-        }];
-        e = function () {
-            return '\\w+'
-        };
-        c = 1
-    };
-    while(c--)
-        if(k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
-    return p
-}('j h=["1f","s","1g","&1h;","1i","&1j;","&1k;","&1l;","&#1m;","<u s="","">","</u>","1n","1o","F","1p","u","1q","F u","<G s="H" 1r="B.o I.J K-L.M.N" 1s="B.o I.J K-L.M.N"><i s="t t-1t"></i> O</G>","1u","1v","1w","1x","1y","1z","1A","1B","1C","1D","1E","1F","1G","O","1H","1I","1J","1K","1L","1M","<i s="t t-1N t-1O"></i> 1P","1Q",".H"];j f=[h[0],h[1],h[2],h[3],h[4],h[5],h[6],h[7],h[8],h[9],h[10],h[11],h[12],h[13],h[14],h[15],h[16],h[17],h[18],h[19],h[P],h[x],h[y],h[Q],h[z],h[A],h[R],h[S],h[m],h[T],h[U],h[v],h[V],h[W],h[X],h[C],h[w],h[D],h[Y],h[Z],h[1a],h[1b],h[1c]];$(l)[f[17]](k(){$(f[14])[f[13]](k(){j a=$(n)[f[0]]();j b=$(n)[f[2]](f[1]);a=a[f[4]](/&/g,f[3]);a=a[f[4]](/</g,f[5]);a=a[f[4]](/>/g,f[6]);a=a[f[4]](/"/g,f[7]);a=a[f[4]](/\'/g,f[8]);$(n)[f[12]](f[9]+b+f[10]+a+f[11])});$(f[16])[f[13]](k(a,b){1R[f[15]](b)})});$(l)[f[17]](k(){j a=$(f[18]),q=f[19];a[f[P]](q)});(k(e){e[f[y]][f[x]]=k(){j a=l;j b=n[0];1S[f[Q]](n,b);r(a[f[A]][f[z]]){j c=l[f[A]][f[z]]();c[f[R]](b);c[f[S]]()}E{r(p[f[m]]){j d=p[f[m]]();j c=l[f[T]]();c[f[U]](b);d[f[v]]();d[f[V]](c);l[f[X]](f[W])}}};e[f[y]][f[C]]=k(){r(p[f[m]]){r(p[f[m]]()[f[w]]){p[f[m]]()[f[w]]()}E{r(p[f[m]]()[f[v]]){p[f[m]]()[f[v]]()}}}E{r(l[f[D]]){l[f[D]][f[w]]()}}}})(1T);$(l)[f[17]](k(){$(f[1c])[f[1b]](k(){j a=$(n)[f[Z]]()[f[Y]](f[16]),q=$(n),1d=f[1a],1e=q[f[12]]();a[f[x]]();q[f[12]](1d);1U(k(){q[f[12]](1e);a[f[C]]()},1V)})})', 62, 120, '|||||||||||||||_0x88fc||_0xb1ca||var|function|document|28|this||window|_0x4332x7|if|class|fa|copy|31|36|21|22|24|25||35|37|else|pre|button|copycode|Tr|ng|IT|www|baotrongit|com|Copy|20|23|26|27|29|30|32|33|34|38|39|||||||||||40|41|42|_0x4332xb|_0x4332xc|text|attr|amp|replace|lt|gt|quot|039|html|each|highlightBlock|ready|alt|title|clipboard|before|selectText|fn|log|createTextRange|body|moveToElementText|select|getSelection|createRange|selectNodeContents|removeAllRanges|addRange|execCommand|removeRange|empty|selection|find|parent|spinner|pulse|Copied|click|hljs|console|jQuery|setTimeout|1000'.split('|'), 0, {}))
+    _0x4332x7.fn.removeRange = function () {
+        if (window.getSelection) {
+            if (window.getSelection().empty) {
+                window.getSelection().empty()
+            } else {
+                if (window.getSelection().removeAllRanges) {
+                    window.getSelection().removeAllRanges()
+                }
+            }
+        } else {
+            if (document.selection) {
+                document.selection.empty()
+            }
+        }
+    }
+})(jQuery);
+$(document).ready(function () {
+    $(".copycode").click(function () {
+        var _0x4332x6 = $(this).parent().find("copy"),
+            _0x4332x7 = $(this),
+            _0x4332xb = "<i class=\"fa fa-spinner fa-pulse\"></i> Copied",
+            _0x4332xc = _0x4332x7.html();
+        _0x4332x6.selectText();
+        _0x4332x7.html(_0x4332xb);
+        setTimeout(function () {
+            _0x4332x7.html(_0x4332xc);
+            _0x4332x6.removeRange()
+        }, 1000)
+    })
+})
